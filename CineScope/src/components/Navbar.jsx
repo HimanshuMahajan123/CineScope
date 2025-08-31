5dbac088cfa63c14b2e2c7317096e8ba446b1928
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2025-08-31 22:48:02 +0530

    Enhance Navbar component with improved styling and structure

--- CineScope/src/components/Navbar.jsx
+++ CineScope/src/components/Navbar.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav className="flex justify-between items-center px-8 py-4 bg-black fixed w-full z-10">
-      <h1 className="text-2xl font-extrabold text-red-600">CineScope</h1>
+     <nav className="flex justify-between items-center px-8 py-4 bg-black fixed w-full z-10 shadow-md">
+      {/* Logo */}
+      <h1 className="text-3xl font-extrabold tracking-wide">
+        <span className="text-red-600">Cine</span>
+        <span className="text-white">Scope</span>
+      </h1>
+
+      {/* Links */}
       <ul className="flex gap-8 text-gray-200 font-medium">
         <li className="hover:text-white cursor-pointer">Home</li>
         <li className="hover:text-white cursor-pointer">Movies</li>
