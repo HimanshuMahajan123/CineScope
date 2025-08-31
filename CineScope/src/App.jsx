d2f928e6aa6ecae91a88fa383774e0c2a5f703f7
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2025-08-31 16:06:33 +0530

    Setup vite and tailwindcss

---
+++ CineScope/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <p className='bg-green-300 text-5xl'>my first step</p>
+    </>
+  )
+}
+
+export default App
