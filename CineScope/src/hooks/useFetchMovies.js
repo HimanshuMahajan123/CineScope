a65679985cf0bc298410ef2a68360927de32db34
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2025-09-06 17:00:47 +0530

    Add useFetchmovies hook for data fetching with loading and error handling

---
+++ CineScope/src/hooks/useFetchMovies.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import React from 'react'
+
+function useFetchmovies(url) {
+  
+    const [data , setData] = useState([]);
+    const [loading , setLoading] = useState(true);
+    const [error , setError] = useState(null);
+
+    useEffect(() => {
+
+        if(!url){
+            return;
+        }
+
+        fetch(url)
+            .then((res) => {
+                if(!res.ok){
+                    throw new Error("failed to fetch data!")
+                }
+                return res.json();
+            })
+            .then((res) => setData(res.results || res))
+            .catch((err) => setError(err.message))
+            .finally(() => setLoading(false))
+    } , [url]);
+
+    return {data , loading , error };
+}
+
+export default useFetchmovies
