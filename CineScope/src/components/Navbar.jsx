2f5599fc52534f2ea8299f4bfed3c59326416121
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2025-09-11 03:22:50 +0530

    Add search functionality to Navbar component; implement useState and useNavigate for query handling

--- CineScope/src/components/Navbar.jsx
+++ CineScope/src/components/Navbar.jsx
@@ -1,19 +1,34 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 
 export default function Navbar() {
   const navItems = [
     { to: "/", label: "Home" },
     { to: "/movies", label: "Movies" },
     { to: "/tv", label: "TV Shows" },
-    { to : "/watchlist" , label : "Watchlist"},
-    { to : "/about" , label : "About"}
+    { to: "/watchlist", label: "Watchlist" },
+    { to: "/about", label: "About" }
   ];
+
+  const [query, setQuery] = useState("")
+  const navigate = useNavigate()
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    if (query.trim()) {
+      navigate(`/search/${query}`)
+      setQuery("")
+    }
+  }
+
 
   return (
     <nav className="fixed top-0 left-0 right-0 bg-black/80 backdrop-blur-md z-50 shadow-lg">
       <div className="container mx-auto flex items-center justify-between px-6 py-4">
-  
+
         <h1 className="text-2xl font-extrabold text-red-600 tracking-wide">CineScope</h1>
+
 
         {/* Nav Links as List */}
         <ul className="flex gap-8">
@@ -22,10 +37,9 @@ export default function Navbar() {
               <NavLink
                 to={item.to}
                 className={({ isActive }) =>
-                  `relative font-medium transition duration-300 ${
-                    isActive
-                      ? "text-red-500 after:content-[''] after:absolute after:-bottom-1 after:left-0 after:w-full after:h-[2px] after:bg-red-500"
-                      : "text-gray-300 hover:text-white"
+                  `relative font-medium transition duration-300 ${isActive
+                    ? "text-red-500 after:content-[''] after:absolute after:-bottom-1 after:left-0 after:w-full after:h-[2px] after:bg-red-500"
+                    : "text-gray-300 hover:text-white"
                   }`
                 }
               >
@@ -34,6 +48,23 @@ export default function Navbar() {
             </li>
           ))}
         </ul>
+
+
+        {/* Searchbar */}
+        <form onSubmit={handleSearch} className="ml-6">
+          <input
+            type="text"
+            placeholder="Search.."
+            value={query}
+            onChange={(e) => { setQuery(e.target.value) }}
+            className="w-3/4 px-4 py-2 rounded-full 
+               bg-white/10 backdrop-blur-md text-white 
+               placeholder-gray-400
+               focus:outline-none "
+          />
+        </form>
+
+
       </div>
     </nav>
   );
